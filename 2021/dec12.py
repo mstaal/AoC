05c845d90d8ac64889e6623fb9bdccbabf506d71
Michael Staal-Olsen <m+github@8i.dk>
2021-12-12 16:15:39 +0100

    Refactoring

--- 2021/dec12.py
+++ 2021/dec12.py
@@ -1,13 +1,6 @@
 from utils import AoCHelper as helper
 
 
-def get_content_map(content):
-    map = {x[0]: set(y[1] for y in content if x[0] == y[0]) for x in content}
-    map.update({x[1]: map.get(x[1], set()).union(set(y[0] for y in content if x[1] == y[1])) for x in content})
-    map.pop("end")
-    return map
-
-
 def generate_paths(map, memory, lower_case_limit):
     if len([elm[-1] for elm in memory if elm[-1] != "end"]) == 0:
         return memory
@@ -20,7 +13,7 @@ def generate_paths(map, memory, lower_case_limit):
         for next in nexts:
             lower_case = [elm for elm in v + [next] if elm.islower()]
             counts = len([lower_case.count(elm) for elm in lower_case if lower_case.count(elm) > 1])
-            if (next.isupper() or counts < lower_case_limit) and next != "start":
+            if next.isupper() or counts < lower_case_limit:
                 next_memory.append(v + [next])
     return generate_paths(map, next_memory, lower_case_limit)
 
@@ -34,7 +27,11 @@ def exercise(map, lowercase_limit):
 
 if __name__ == '__main__':
     content = [element.split("-") for element in helper.splitFile("day12.txt", "\n")]
-    map = get_content_map(content)
+    map = {x[0]: set(y[1] for y in content if x[0] == y[0]) for x in content}
+    map.update({x[1]: map.get(x[1], set()).union(set(y[0] for y in content if x[1] == y[1])) for x in content})
+    for key, value in map.items():
+        map[key] = set(filter(lambda x: x != "start", value))
+    map.pop("end")
     print(f"Result 1: {str(exercise(map, 2))}")
 
     print(f"Result 2: {str(exercise(map, 3))}")
