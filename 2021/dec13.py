e224f0c262a614106fb076e6e0b1fdf2c8f9657a
Michael Staal-Olsen <m+github@8i.dk>
2021-12-13 09:36:06 +0100

    Use sets

--- 2021/dec13.py
+++ 2021/dec13.py
@@ -5,37 +5,34 @@ import matplotlib.pyplot as plt
 
 def exercise(coordinates, folds):
     for c, size in folds:
-        temp = {}
-        for x, y in coordinates.keys():
+        temp = set()
+        for x, y in coordinates:
             if c == "y":
                 if y > size:
                     new_y = size - abs(y-size)
-                    temp[(x, new_y)] = True
-                    temp[(x, y)] = False
+                    temp.add((x, new_y))
                 else:
-                    temp[(x, y)] = True
+                    temp.add((x, y))
             if c == "x":
                 if x > size:
                     new_x = size - abs(x-size)
-                    temp[(new_x, y)] = True
-                    temp[(x, y)] = False
+                    temp.add((new_x, y))
                 else:
-                    temp[(x, y)] = True
-        coordinates.update(temp)
-        coordinates = {k: v for k, v in coordinates.items() if v}
+                    temp.add((x, y))
+        coordinates = temp
     return coordinates
 
 
 def plot(coordinates, folds):
     result = exercise(coordinates, folds)
-    points = [np.array(p).dot([[1, 0], [0, -1]]) for p in list(result.keys())]
+    points = [np.array(p).dot([[1, 0], [0, -1]]) for p in list(result)]
     plt.scatter([x for x, y in points], [y for x, y in points])
     plt.show()
 
 
 if __name__ == '__main__':
     coordinates_content, fold_content = [element for element in helper.splitFile("day13.txt", "\n\n")]
-    coordinates = {(int(elm.split(",")[0]), int(elm.split(",")[1])): True for elm in coordinates_content.split("\n")}
+    coordinates = set((int(elm.split(",")[0]), int(elm.split(",")[1])) for elm in coordinates_content.split("\n"))
     folds = [(c[-1], int(number)) for c, number in [elm.split("=") for elm in fold_content.split("\n")]]
 
     print(f"Result 1: {str(len(exercise(coordinates, folds[0:1])))}")
