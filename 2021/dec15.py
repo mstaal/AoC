4f0021d81c78d108121530648821866770d89948
Michael Staal-Olsen <m+github@8i.dk>
2021-12-15 12:27:10 +0100

    renamings

--- 2021/dec15.py
+++ 2021/dec15.py
@@ -8,13 +8,13 @@ from utils.abcTypes import Dijkstra
 def exercise(content):
     dijk_graph = {}
     for idx, elm_x in enumerate(content):
-        for idy, elm_y in enumerate(elm_x):
+        for idy, dist_back in enumerate(elm_x):
             adjacent = helper.get_neighbours(content, idx, idy, cardinal_directions, True)
-            for adj, value in adjacent.items():
+            for adj, dist_to_adj in adjacent.items():
                 dijk_graph[(idx, idy)] = dijk_graph.get((idx, idy), {})
                 dijk_graph[adj] = dijk_graph.get(adj, {})
-                dijk_graph[(idx, idy)][adj] = value
-                dijk_graph[adj][(idx, idy)] = elm_y
+                dijk_graph[(idx, idy)][adj] = dist_to_adj
+                dijk_graph[adj][(idx, idy)] = dist_back
     dijk = Dijkstra(dijk_graph)
     path_length = dijk.dijkstra((0, 0))
     result = path_length[(len(content)-1, len(content)-1)]
