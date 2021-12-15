eef40715d2a330e6d3fdcbe9d76bfdf11f0aba9e
Michael Staal-Olsen <m+github@8i.dk>
2021-12-15 12:46:40 +0100

    refactoring

--- 2020/dec18.py
+++ 2020/dec18.py
@@ -1,22 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import time
-from collections import Counter
-
-import pandas as pd
-import AoCHelper as helper
-from AoCHelper import Infix
-import numpy as np
-import sympy
-import itertools
-import re
-import copy as cc
-from functools import reduce
-from itertools import permutations, combinations, chain, product
-
-signLambda = lambda letter: 1 if letter == "R" else -1
+import utils.AoCHelper as helper
+from utils.abcTypes import Infix
 
 
 # https://code.activestate.com/recipes/384122/
@@ -25,17 +8,9 @@ mul = Infix(lambda x, y: x * y)
 add = Infix(lambda x, y: x + y)
 
 
-
-def calculate():
-    # Use a breakpoint in the code line below to debug your script.
+if __name__ == '__main__':
     content = helper.splitFile("day18.txt", "\n")
     firstExercise = sum([eval(element.replace("+", "|add|").replace("*", "|mul|")) for element in content])
     secondExercise = sum([eval(element.replace("*", "|mul|")) for element in content])
-
-    hj = ""
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    calculate()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(f"Result 1: {str(firstExercise)}")
+    print(f"Result 3: {str(secondExercise)}")
