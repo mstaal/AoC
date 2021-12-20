b6255369b6d0fe7181dce8a38cbad683bddeadcd
Michael Staal-Olsen <m+github@8i.dk>
2021-12-20 14:43:34 +0100

    Cleanup

--- 2021/dec17.py
+++ 2021/dec17.py
@@ -12,7 +12,7 @@ def move(velocity, area):
     point = (0, 0)
     if area[0][0] <= point[0] <= area[0][1] and area[1][0] <= point[1] <= area[1][1]:
         return point, 0, velocity, True
-    t = 0
+    t = 1
     while True:
         vel = velocity_at_time(velocity, t)
         point = (point[0] + vel[0], point[1] + vel[1])
@@ -24,16 +24,18 @@ def move(velocity, area):
         t += 1
 
 
+def exercise1(area, velocities):
+    results = [move(vel, area) for vel in velocities]
+    results_filtered = [x for x in results if x[3]]
+    return max([r[1] for r in results_filtered])
+
 
 if __name__ == '__main__':
     content = helper.splitFile("day17.txt", "\n")
     input = [elm[2:].split("..") for elm in [element.replace("target area: ", "").split(", ") for element in content][0]]
     area = ((int(input[0][0]), int(input[0][1])), (int(input[1][0]), int(input[1][1])))
     velocities = [(x, y) for x in range(0, area[0][1]) for y in range(-abs(area[1][0]), abs(area[1][0]))]
-    results = [move(vel, area) for vel in velocities]
-    results_filtered = [x for x in results if x[3]]
-    correct = [res for res in results_filtered if res[1] == max([r[1] for r in results_filtered])]
-    res = move((7, 2), area)
+    res1 = exercise1(area, velocities)
+    print(f"Result 1: {str(res1)}")
 
-    print(f"Result 1: {str(move((7, 2)))}")
 
