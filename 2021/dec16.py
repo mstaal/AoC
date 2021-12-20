937a739d4fa948da143c84a66f2df51635a61b16
Michael Staal-Olsen <m+github@8i.dk>
2021-12-20 18:59:21 +0100

    Extra simplification

--- 2021/dec16.py
+++ 2021/dec16.py
@@ -3,35 +3,34 @@ from utils import AoCHelper as helper
 
 
 class Packet:
-    def __init__(self, packet_version, type_id, tail):
+    def __init__(self, packet_version, tail):
         self.packet_version = packet_version
-        self.type_id = type_id
         self.tail = tail
 
 
 class Literal(Packet):
-    def __init__(self, packet_version, type_id, value, tail):
-        super().__init__(packet_version, type_id, tail)
+    def __init__(self, packet_version, value, tail):
+        super().__init__(packet_version, tail)
         self.value = value
 
     def get_value(self):
         return self.value
 
     def __repr__(self):
-        return f"(Type: {self.type_id}, Version: {self.packet_version}, Value: {self.value})"
+        return f"(Version: {self.packet_version}, Value: {self.value})"
 
     def get_version_sum(self):
         return self.packet_version
 
 
 class Operator(Packet):
-    def __init__(self, packet_version, type_id, operator_val, tail):
-        super().__init__(packet_version, type_id, tail)
+    def __init__(self, packet_version, operator_val, tail):
+        super().__init__(packet_version, tail)
         self.operator_val = operator_val
         self.value = []
 
     def __repr__(self):
-        return f"(Type: {self.type_id}, Version: {self.packet_version})"
+        return f"(Version: {self.packet_version})"
 
     def get_version_sum(self):
         return self.packet_version + sum(val.get_version_sum() for val in self.value)
@@ -41,56 +40,56 @@ class Operator(Packet):
 
 
 class Sum(Operator):
-    def __init__(self, packet_version, type_id, operator_val, tail):
-        super().__init__(packet_version, type_id, operator_val, tail)
+    def __init__(self, packet_version, operator_val, tail):
+        super().__init__(packet_version, operator_val, tail)
 
     def get_value(self):
         return sum([element.get_value() for element in self.value])
 
 
 class Product(Operator):
-    def __init__(self, packet_version, type_id, operator_val, tail):
-        super().__init__(packet_version, type_id, operator_val, tail)
+    def __init__(self, packet_version, operator_val, tail):
+        super().__init__(packet_version, operator_val, tail)
 
     def get_value(self):
         return reduce(lambda x, y: x*y, [element.get_value() for element in self.value])
 
 
 class Minimum(Operator):
-    def __init__(self, packet_version, type_id, operator_val, tail):
-        super().__init__(packet_version, type_id, operator_val, tail)
+    def __init__(self, packet_version, operator_val, tail):
+        super().__init__(packet_version, operator_val, tail)
 
     def get_value(self):
         return min([element.get_value() for element in self.value])
 
 
 class Maximum(Operator):
-    def __init__(self, packet_version, type_id, operator_val, tail):
-        super().__init__(packet_version, type_id, operator_val, tail)
+    def __init__(self, packet_version, operator_val, tail):
+        super().__init__(packet_version, operator_val, tail)
 
     def get_value(self):
         return max([element.get_value() for element in self.value])
 
 
 class Greater(Operator):
-    def __init__(self, packet_version, type_id, operator_val, tail):
-        super().__init__(packet_version, type_id, operator_val, tail)
+    def __init__(self, packet_version, operator_val, tail):
+        super().__init__(packet_version, operator_val, tail)
 
     def get_value(self):
         return 1 if self.value[0].get_value() > self.value[1].get_value() else 0
 
 
 class Less(Operator):
-    def __init__(self, packet_version, type_id, operator_val, tail):
-        super().__init__(packet_version, type_id, operator_val, tail)
+    def __init__(self, packet_version, operator_val, tail):
+        super().__init__(packet_version, operator_val, tail)
 
     def get_value(self):
         return 1 if self.value[0].get_value() < self.value[1].get_value() else 0
 
 
 class Equal(Operator):
-    def __init__(self, packet_version, type_id, operator_val, tail):
-        super().__init__(packet_version, type_id, operator_val, tail)
+    def __init__(self, packet_version, operator_val, tail):
+        super().__init__(packet_version, operator_val, tail)
 
     def get_value(self):
         return 1 if self.value[0].get_value() == self.value[1].get_value() else 0
@@ -103,19 +102,19 @@ def hex_to_bin(h):
 
 def buildOperator(packet_version, type_id, length_val, tail):
     if type_id == 0:
-        return Sum(packet_version, type_id, length_val, tail)
+        return Sum(packet_version, length_val, tail)
     elif type_id == 1:
-        return Product(packet_version, type_id, length_val, tail)
+        return Product(packet_version, length_val, tail)
     elif type_id == 2:
-        return Minimum(packet_version, type_id, length_val, tail)
+        return Minimum(packet_version, length_val, tail)
     elif type_id == 3:
-        return Maximum(packet_version, type_id, length_val, tail)
+        return Maximum(packet_version, length_val, tail)
     elif type_id == 5:
-        return Greater(packet_version, type_id, length_val, tail)
+        return Greater(packet_version, length_val, tail)
     elif type_id == 6:
-        return Less(packet_version, type_id, length_val, tail)
+        return Less(packet_version, length_val, tail)
     elif type_id == 7:
-        return Equal(packet_version, type_id, length_val, tail)
+        return Equal(packet_version, length_val, tail)
 
 
 def generate_packet(binary):
@@ -130,7 +129,7 @@ def generate_packet(binary):
         pieces.append(remainder[1:5])
         remainder = remainder[5:]
         value = int(''.join(pieces)[5:], 2)
-        packet = Literal(packet_version, type_id, value, remainder)
+        packet = Literal(packet_version, value, remainder)
     else:
         length_type = int(binary[6])
         if length_type == 0:
