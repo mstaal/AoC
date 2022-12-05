f9cc727fd76b9370cf0deba3cd2f06ee179dd8f2
Michael <m@8.dk>
2022-12-05 09:49:57 +0100

    Refactor

--- 2022/dec5.py
+++ 2022/dec5.py
@@ -7,12 +7,9 @@ def parse_crate_structure(crate_raw_input):
     crate_map = dict()
     splitting = crate_raw_input.split("\n")
     lines = [e.split(" ") for e in splitting]
-    chars = lines[:-1]
     numbers = [int(e) for e in splitting[-1].split(" ") if e.isdigit()]
     for n in numbers:
-        column = [c[n-1][1] for c in chars if c[n-1] != "..."]
-        column.reverse()
-        crate_map[n] = column
+        crate_map[n] = list(reversed([c[n-1][1] for c in lines[:-1] if c[n-1] != "..."]))
     return crate_map
 
 
@@ -22,27 +19,16 @@ def parse_procedures(procedures_raw_input):
 
 
 @helper.profiler
-def part1(crate_input, procedures_input):
-    crate_copy = deepcopy(crate_input)
+def calculate_crate_distribution(crate_input, procedures_input, reverse_tail):
+    crate_map = deepcopy(crate_input)
     for amount, from_, to_ in procedures_input:
-        from_list = crate_copy[from_]
-        to_list = crate_copy[to_]
-        for _ in range(0, amount):
-            to_list.append(from_list.pop())
-    return crate_copy
-
-
-@helper.profiler
-def part2(crate_input, procedures_input):
-    crate_copy = deepcopy(crate_input)
-    for amount, from_, to_ in procedures_input:
-        from_list = crate_copy[from_]
-        to_list = crate_copy[to_]
+        from_list = crate_map[from_]
+        to_list = crate_map[to_]
         from_remaining = from_list[:-amount]
-        from_transfer = from_list[-amount:]
-        crate_copy[from_] = from_remaining
-        crate_copy[to_] = to_list + from_transfer
-    return crate_copy
+        from_transfer = (list(reversed(from_list[-amount:])) if reverse_tail else from_list[-amount:])
+        crate_map[from_] = from_remaining
+        crate_map[to_] = to_list + from_transfer
+    return crate_map
 
 
 if __name__ == '__main__':
@@ -50,9 +36,9 @@ if __name__ == '__main__':
     crates = parse_crate_structure(crates_raw)
     procedures = parse_procedures(procedures_raw)
 
-    def joining(result: dict): return "".join([val[-1] for val in result.values()])
-    question1 = joining(part1(crates, procedures))
-    question2 = joining(part2(crates, procedures))
+    def joining(result: dict): return "".join(val[-1] for val in result.values())
+    question1 = joining(calculate_crate_distribution(crates, procedures, True))
+    question2 = joining(calculate_crate_distribution(crates, procedures, False))
 
     print(f"Result 1: {str(question1)}")
     print(f"Result 2: {str(question2)}")
