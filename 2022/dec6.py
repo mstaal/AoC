07f13f6486fd8600860ad4e0e50268d4c2200548
Michael <m@8.dk>
2022-12-06 07:30:28 +0100

    Refactor

--- 2022/dec6.py
+++ 2022/dec6.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 @helper.profiler
-def part1(content_list, seq_length):
+def solve(content_list, seq_length):
     for idx, _ in enumerate(content_list):
         if len(set(content_list[idx: idx + seq_length])) == seq_length:
             return idx + seq_length
@@ -12,8 +12,5 @@ def part1(content_list, seq_length):
 if __name__ == '__main__':
     content = list(Path("data/day6.txt").read_text())
 
-    question1 = part1(content, 4)
-    question2 = part1(content, 14)
-
-    print(f"Result 1: {str(question1)}")
-    print(f"Result 2: {str(question2)}")
+    print(f"Result 1: {str(solve(content, 4))}")
+    print(f"Result 2: {str(solve(content, 14))}")
