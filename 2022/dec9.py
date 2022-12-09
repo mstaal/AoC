61161548943fdf03a057fad3924ff60963245a13
Michael <m@8.dk>
2022-12-10 00:51:33 +0100

    Refactor

--- 2022/dec9.py
+++ 2022/dec9.py
@@ -12,31 +12,6 @@ diagonal_directions = {T(1, 1), T(1, -1), T(-1, 1), T(-1, -1)}
 
 
 @helper.profiler
-def part1(content_list):
-    tail_memory = {T(0, 0)}
-    head_position = T(0, 0)
-    tail_position = T(0, 0)
-
-    for direction, count in content_list:
-        direction_vector = direction_map[direction]
-        for _ in range(count):
-            head_position += direction_vector
-            diff_vector = head_position - tail_position
-            distance = diff_vector.length()
-            if distance > max_allowed_distance:
-                if diff_vector in [T(2, 0), T(2, 1), T(2, -1)]:
-                    tail_position = T(head_position[0]-1, head_position[1])
-                elif diff_vector in [T(0, 2), T(1, 2), T(-1, 2)]:
-                    tail_position = T(head_position[0], head_position[1]-1)
-                elif diff_vector in [T(-2, 0), T(-2, 1), T(-2, -1)]:
-                    tail_position = T(head_position[0]+1, head_position[1])
-                elif diff_vector in [T(0, -2), T(1, -2), T(-1, -2)]:
-                    tail_position = T(head_position[0], head_position[1]+1)
-                tail_memory.add(tail_position)
-    return len(tail_memory)
-
-
-@helper.profiler
 def calculate_unique_tail_visits(content_list, rope_length):
     tail_memory = {i: {T(0, 0)} for i in range(1, rope_length+1)}
     rope = [T(0, 0) for _ in range(0, rope_length+1)]
