98fb8c4671891a5338487a535bc5ffb02c8c9821
Michael <m@8.dk>
2022-12-11 09:08:34 +0100

    Part 2

--- 2022/dec11.py
+++ 2022/dec11.py
@@ -1,5 +1,8 @@
+import copy
 from utils import aoc_helper as helper
 from pathlib import Path
+from operator import mul
+from functools import reduce
 
 
 def parse_line(element):
@@ -7,8 +10,8 @@ def parse_line(element):
     return {
         "monkey": int(lines[0].split(" ")[1][:-1]),
         "worry_levels": [int(e) for e in lines[1].split(": ")[1].split(", ")],
-        "operation_new": lines[2].split(" = ")[1],
-        "divisible_test": int(lines[3].split(" by ")[1]),
+        "operation": lines[2].split(" = ")[1],
+        "divisor": int(lines[3].split(" by ")[1]),
         "if_true": int(lines[4].split(" to monkey ")[1]),
         "if_false": int(lines[5].split(" to monkey ")[1]),
         "inspected_count": 0
@@ -16,7 +19,9 @@ def parse_line(element):
 
 
 @helper.profiler
-def part1(monkey_mapping, rounds=20, divide=3):
+def calculate_monkey_business(monkey_mapping_input, rounds, divide=1):
+    monkey_mapping = copy.deepcopy(monkey_mapping_input)
+    lcm = reduce(mul, [monkey["divisor"] for monkey in monkey_mapping.values()], 1)
     for r in range(0, rounds):
         for idx in range(0, len(monkey_mapping)):
             current_monkey = monkey_mapping[idx]
@@ -24,26 +29,18 @@ def part1(monkey_mapping, rounds=20, divide=3):
             current_monkey["worry_levels"] = []
             for level in levels:
                 current_monkey["inspected_count"] += 1
-                operation = eval(current_monkey["operation_new"].replace("old", str(level))) // divide
-                remainder = operation % current_monkey["divisible_test"]
-                divisible_test = remainder == 0
-                next_monkey = monkey_mapping[
-                    current_monkey["if_true"] if divisible_test else current_monkey["if_false"]]
+                # (a mod k*n) mod n == a mod n where k*n == 'lcm'. Relevant for part 2.
+                operation = eval(f'{current_monkey["operation"].replace("old", str(level))} % {lcm}') // divide
+                remainder_test = operation % current_monkey["divisor"] == 0
+                next_monkey = monkey_mapping[current_monkey["if_true"] if remainder_test else current_monkey["if_false"]]
                 next_monkey["worry_levels"].append(operation)
-    inspection_counts = [monkey["inspected_count"] for _, monkey in monkey_mapping.items()]
-    max_a, max_b = sorted(inspection_counts)[-2:]
-    monkey_business = max_a * max_b
+    monkey_business = reduce(mul, sorted([monkey["inspected_count"] for monkey in monkey_mapping.values()])[-2:], 1)
     return monkey_business
-
-
-@helper.profiler
-def part2(content_list):
-    return "text"
 
 
 if __name__ == '__main__':
     content = [parse_line(element) for element in Path("data/day11.txt").read_text().split("\n\n")]
     monkey_map = {element["monkey"]: element for element in content}
 
-    print(f"Result 1: {str(part1(monkey_map))}")
-    print(f"Result 2: {str(part2(content))}")
+    print(f"Result 1: {str(calculate_monkey_business(monkey_map, 20, 3))}")
+    print(f"Result 2: {str(calculate_monkey_business(monkey_map, 10000))}")
