6c770a9cb44c5439b28b2a52c0fc31b9f7877729
Michael <m@8.dk>
2022-12-12 09:52:27 +0100

    Refactor

--- 2022/dec12.py
+++ 2022/dec12.py
@@ -19,17 +19,17 @@ def get_dijkstra_graph(content_list, weight_lambda):
 
 
 @helper.profiler
-def part1(content_list, start, end):
-    dijk = get_dijkstra_graph(content_list, lambda val, adj_val: adj_val in range(0, val + 2))
-    path_length_from_start = dijk.dijkstra(start)
-    shortest_length_to_end = path_length_from_start[end]
-    return shortest_length_to_end
+def part1(numeric, start, end):
+    graph = get_dijkstra_graph(numeric, lambda val, adj_val: adj_val in range(0, val + 2))
+    shortest_length_from_start_to_end = graph.dijkstra(start)[end]
+    return shortest_length_from_start_to_end
 
 
 @helper.profiler
-def part2(map_numeric_mapping, map_numeric, end):
-    possible_starts = {a for a, val in map_numeric_mapping.items() if val == 0}
-    shortest_paths_from_starts = {start: part1(map_numeric, start, end) for start in possible_starts}
+def part2(numeric, end):
+    numeric_mapping = {(idx, idy): val for idx, row in enumerate(numeric) for idy, val in enumerate(row)}
+    possible_starts = {a for a, val in numeric_mapping.items() if val == 0}
+    shortest_paths_from_starts = {start: part1(numeric, start, end) for start in possible_starts}
     minimal_choice = min(shortest_paths_from_starts.values())
     return minimal_choice
 
@@ -37,10 +37,8 @@ def part2(map_numeric_mapping, map_numeric, end):
 if __name__ == '__main__':
     content = [[d for d in e] for e in Path("data/day12.txt").read_text().split("\n")]
     start_end = {val: (idx, idy) for idx, e in enumerate(content) for idy, val in enumerate(e) if val in ["S", "E"]}
-    map_raw = [[d.replace("E", "z").replace("S", "a") for d in e] for e in content]
-    to_numeric = [[ord(d)-97 for d in e] for e in map_raw]
-    to_numeric_dict = {(idx, idy): val for idx, row in enumerate(to_numeric) for idy, val in enumerate(row)}
+    numeric_repr = [[ord(d)-97 for d in e] for e in [[d.replace("E", "z").replace("S", "a") for d in e] for e in content]]
 
-    print(f"Result 1: {str(part1(to_numeric, start_end['S'], start_end['E']))}")
+    print(f"Result 1: {str(part1(numeric_repr, start_end['S'], start_end['E']))}")
 
-    print(f"Result 2: {str(part2(to_numeric_dict, to_numeric, start_end['E']))}")
+    print(f"Result 2: {str(part2(numeric_repr, start_end['E']))}")
