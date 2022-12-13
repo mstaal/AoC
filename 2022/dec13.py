158df2b2d52fd72fa53a52366178bc2f9055c8a5
Michael <m@8.dk>
2022-12-14 00:38:09 +0100

    Refactor

--- 2022/dec13.py
+++ 2022/dec13.py
@@ -5,15 +5,11 @@ from functools import cmp_to_key
 import ast
 
 
-def parse(lst):
-    return [ast.literal_eval(e) for e in lst]
-
-
 def compare_left_right(left, right):
-    return compare_left_right_wrapper(deepcopy(left), deepcopy(right))
+    return -1 if __compare_left_right(deepcopy(left), deepcopy(right)) else 1
 
 
-def compare_left_right_wrapper(left, right):
+def __compare_left_right(left, right):
     if len(left) == 0 and len(right) > 0:
         return True
     elif len(left) > 0 and len(right) == 0:
@@ -30,17 +26,17 @@ def compare_left_right_wrapper(left, right):
         return a <= b
     elif isinstance(a, int) and isinstance(b, int):
         if a == b:
-            return compare_left_right_wrapper(left, right)
+            return __compare_left_right(left, right)
         return a < b
     elif isinstance(a, list) and isinstance(b, list):
-        preliminary = compare_left_right_wrapper(a, b)
+        preliminary = __compare_left_right(a, b)
         if preliminary is not None:
             return preliminary
-        return compare_left_right_wrapper(left, right)
+        return __compare_left_right(left, right)
     elif isinstance(a, int) and isinstance(b, list):
-        return compare_left_right_wrapper([[a]] + left, [b] + right)
+        return __compare_left_right([[a]] + left, [b] + right)
     elif isinstance(a, list) and isinstance(b, int):
-        return compare_left_right_wrapper([a] + left, [[b]] + right)
+        return __compare_left_right([a] + left, [[b]] + right)
 
 
 @helper.profiler
@@ -48,19 +44,18 @@ def part1(content_list):
     right_order_map = {}
     for idx, (first, second) in enumerate(content_list):
         right_order_map[idx] = compare_left_right(first, second)
-    indices_sum = sum(idx+1 for idx, val in right_order_map.items() if val)
-    return indices_sum
+    return sum(idx+1 for idx, val in right_order_map.items() if val == -1)
 
 
 @helper.profiler
 def part2(content_input):
-    def compare(left, right): return -1 if compare_left_right(left, right) else 1
-    sorted_list = sorted(content_input, key=cmp_to_key(compare))
+    sorted_list = sorted(content_input, key=cmp_to_key(compare_left_right))
     first, second = [idx+1 for idx, lst in enumerate(sorted_list) if lst == [[2]] or lst == [[6]]]
     return first * second
 
 
 if __name__ == '__main__':
+    def parse(lst): return [ast.literal_eval(e) for e in lst]
     content = [e.split("\n") for e in Path("data/day13.txt").read_text().split("\n\n")]
     content_parsed = [parse(lst) for lst in content]
 
