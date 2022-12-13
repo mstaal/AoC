483b4064e5182791171797146f0fdb561f86c791
Michael <m@8.dk>
2022-12-14 00:31:06 +0100

    Minor cleanup

--- 2022/dec13.py
+++ 2022/dec13.py
@@ -9,10 +9,6 @@ def parse(lst):
     return [ast.literal_eval(e) for e in lst]
 
 
-def compare(left, right):
-    return -1 if compare_left_right(left, right) else 1
-
-
 def compare_left_right(left, right):
     return compare_left_right_wrapper(deepcopy(left), deepcopy(right))
 
@@ -51,27 +47,24 @@ def compare_left_right_wrapper(left, right):
 def part1(content_list):
     right_order_map = {}
     for idx, (first, second) in enumerate(content_list):
-        result = compare_left_right(first, second)
-        right_order_map[idx] = result
+        right_order_map[idx] = compare_left_right(first, second)
     indices_sum = sum(idx+1 for idx, val in right_order_map.items() if val)
     return indices_sum
 
 
 @helper.profiler
 def part2(content_input):
+    def compare(left, right): return -1 if compare_left_right(left, right) else 1
     sorted_list = sorted(content_input, key=cmp_to_key(compare))
     first, second = [idx+1 for idx, lst in enumerate(sorted_list) if lst == [[2]] or lst == [[6]]]
-    prod = first * second
-    return prod
+    return first * second
 
 
 if __name__ == '__main__':
     content = [e.split("\n") for e in Path("data/day13.txt").read_text().split("\n\n")]
     content_parsed = [parse(lst) for lst in content]
 
-    content_parsed_part_2 = [e for lst in deepcopy(content_parsed) for e in lst]
-    content_parsed_part_2.append([[2]])
-    content_parsed_part_2.append([[6]])
+    content_parsed_part_2 = [e for lst in deepcopy(content_parsed) for e in lst] + [[[2]]] + [[[6]]]
 
     print(f"Result 1: {str(part1(content_parsed))}")
     print(f"Result 2: {str(part2(content_parsed_part_2))}")
