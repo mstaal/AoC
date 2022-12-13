a3b76ddecaee5e5edf74b2c99da9d95f406c7649
Michael <m@8.dk>
2022-12-14 00:26:01 +0100

    dec13 part2

--- 2022/dec13.py
+++ 2022/dec13.py
@@ -1,5 +1,7 @@
 from utils import aoc_helper as helper
 from pathlib import Path
+from copy import deepcopy
+from functools import cmp_to_key
 import ast
 
 
@@ -7,7 +9,15 @@ def parse(lst):
     return [ast.literal_eval(e) for e in lst]
 
 
+def compare(left, right):
+    return -1 if compare_left_right(left, right) else 1
+
+
 def compare_left_right(left, right):
+    return compare_left_right_wrapper(deepcopy(left), deepcopy(right))
+
+
+def compare_left_right_wrapper(left, right):
     if len(left) == 0 and len(right) > 0:
         return True
     elif len(left) > 0 and len(right) == 0:
@@ -24,17 +34,17 @@ def compare_left_right(left, right):
         return a <= b
     elif isinstance(a, int) and isinstance(b, int):
         if a == b:
-            return compare_left_right(left, right)
+            return compare_left_right_wrapper(left, right)
         return a < b
     elif isinstance(a, list) and isinstance(b, list):
-        preliminary = compare_left_right(a, b)
+        preliminary = compare_left_right_wrapper(a, b)
         if preliminary is not None:
             return preliminary
-        return compare_left_right(left, right)
+        return compare_left_right_wrapper(left, right)
     elif isinstance(a, int) and isinstance(b, list):
-        return compare_left_right([[a]] + left, [b] + right)
+        return compare_left_right_wrapper([[a]] + left, [b] + right)
     elif isinstance(a, list) and isinstance(b, int):
-        return compare_left_right([a] + left, [[b]] + right)
+        return compare_left_right_wrapper([a] + left, [[b]] + right)
 
 
 @helper.profiler
@@ -48,15 +58,20 @@ def part1(content_list):
 
 
 @helper.profiler
-def part2(content_list):
-    return ""
+def part2(content_input):
+    sorted_list = sorted(content_input, key=cmp_to_key(compare))
+    first, second = [idx+1 for idx, lst in enumerate(sorted_list) if lst == [[2]] or lst == [[6]]]
+    prod = first * second
+    return prod
 
 
 if __name__ == '__main__':
     content = [e.split("\n") for e in Path("data/day13.txt").read_text().split("\n\n")]
     content_parsed = [parse(lst) for lst in content]
 
+    content_parsed_part_2 = [e for lst in deepcopy(content_parsed) for e in lst]
+    content_parsed_part_2.append([[2]])
+    content_parsed_part_2.append([[6]])
 
     print(f"Result 1: {str(part1(content_parsed))}")
-
-    print(f"Result 2: {str(part2(content_parsed))}")
+    print(f"Result 2: {str(part2(content_parsed_part_2))}")
