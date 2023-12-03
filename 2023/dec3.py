ac1e3bc9b2c7945dc826f7253bda9b55a45ac9de
mstaal <m@8.dk>
2023-12-03 13:51:44 +0100

    Day 3, part 1

--- 2023/dec3.py
+++ 2023/dec3.py
@@ -1,9 +1,7 @@
-from typing import List, Tuple
-
 from utils import aoc_helper as helper
 from pathlib import Path
-import numpy as np
 import re
+from collections import defaultdict
 
 special_characters = {"#", "$", "%", "&", "*", "+", "-", "/", "=", "@"}
 
@@ -16,18 +14,32 @@ if __name__ == '__main__':
     characters_dict = {(idx, idy): y for idx, x in enumerate(content) for idy, y in enumerate(x)}
 
     number_matches = [[(idx, m.span(), m.group()) for m in re.compile(r"\d+").finditer(e)] for idx, e in enumerate(content)]
-    flattened_matches = [item for lst in number_matches for item in lst]
+    flattened_number_matches = [item for lst in number_matches for item in lst]
 
     symbol_matches_dict = dict()
-    for idx, (y_start, y_end), match in flattened_matches:
+    for idx, (y_start, y_end), number_match in flattened_number_matches:
         for idy in range(y_start, y_end):
             neighbours = helper.get_neighbours_dict(content, idx, idy, ignore_none=True)
             symbol_matches = set(neighbours.values()).intersection(special_characters)
             if len(symbol_matches) > 0:
-                symbol_matches_dict[(idx, (y_start, y_end))] = match
+                symbol_matches_dict[(idx, (y_start, y_end))] = number_match
 
     question1 = sum([int(x) for x in symbol_matches_dict.values()])
 
-    question2 = question_2(games_parsed)
+
+    star_matches_dict = dict()
+    for idx, (y_start, y_end), number_match in flattened_number_matches:
+        for idy in range(y_start, y_end):
+            neighbours = helper.get_neighbours_dict(content, idx, idy, ignore_none=True)
+            for (n_x, n_y), character in neighbours.items():
+                if character == "*":
+                    star_matches_dict[(idx, (y_start, y_end), number_match)] = (n_x, n_y)
+
+    groups = defaultdict(list)
+    for x, y in star_matches_dict.items():
+        groups[y].append(x)
+
+    question2 = sum([int(x[0][2]) * int(x[1][2]) for x in groups.values() if len(x) == 2])
+
     print(f"Result 1: {str(question1)}")
     print(f"Result 2: {str(question2)}")
