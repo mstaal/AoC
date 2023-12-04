69c770f4df22b0034b6aec5d3498cb6381872972
mstaal <m@8.dk>
2023-12-04 19:14:42 +0100

    Day 4, refactor

--- 2023/dec4.py
+++ 2023/dec4.py
@@ -2,30 +2,29 @@ from utils import aoc_helper as helper
 from pathlib import Path
 
 
-def parse_line(line) -> tuple[int, list[int], list[int]]:
+def parse_line(line) -> tuple[int, int]:
     splitted = line.split(":")
     card = int(splitted[0].replace("Card ", "").strip())
     winning_numbers, my_numbers = tuple([e.strip().split(" ") for e in splitted[1].strip().split("|")])
     winning_numbers, my_numbers = [int(e) for e in winning_numbers if e != ""], [int(e) for e in my_numbers if e != ""]
-    return card, winning_numbers, my_numbers
+    number_of_winning_numbers = len(set(winning_numbers).intersection(my_numbers))
+    return card, number_of_winning_numbers
 
 
-def question_1(inp: list[tuple[int, list[int], list[int]]]) -> int:
+def question_1(inp: list[tuple[int, int]]) -> int:
     points_overview = []
-    for card, winning_numbers, my_numbers in inp:
-        length = len(set(winning_numbers).intersection(my_numbers))
-        points = pow(2, length - 1) if length > 0 else 0
+    for card, number_of_winning_numbers in inp:
+        points = pow(2, number_of_winning_numbers - 1) if number_of_winning_numbers > 0 else 0
         points_overview.append(points)
     return sum(points_overview)
 
 
-def question_2(inp: list[tuple[int, list[int], list[int]]]) -> int:
+def question_2(inp: list[tuple[int, int]]) -> int:
     card_occurence_count = [1] * len(inp)
-    for card_no, winning_numbers, my_numbers in inp:
-        next_numbers = range(card_no+1, card_no + len(set(winning_numbers).intersection(my_numbers))+1)
+    for card_no, number_of_winning_numbers in inp:
+        next_numbers = range(card_no+1, card_no+1+number_of_winning_numbers)
         for n in next_numbers:
             card_occurence_count[n-1] += card_occurence_count[card_no-1]
-
     return sum(card_occurence_count)
 
 
