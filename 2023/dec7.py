04cd03d0f4e6e21ad755c2b2eaef31a00a29d415
mstaal <m@8.dk>
2023-12-07 12:54:36 +0100

    Day 7, q1, refactor

--- 2023/dec7.py
+++ 2023/dec7.py
@@ -5,8 +5,7 @@ import numpy as np
 from collections import Counter
 
 
-ORDER = {"A": 1, "K": 2, "Q": 3, "J": 4, "T": 5, "9": 6, "8": 7, "7": 8, "6": 9, "5": 10, "4": 11, "3": 12, "2": 13}
-ORDER_J = {"A": 1, "K": 2, "Q": 3, "J": 14, "T": 5, "9": 6, "8": 7, "7": 8, "6": 9, "5": 10, "4": 11, "3": 12, "2": 13}
+ORDER = {"A": 13, "K": 12, "Q": 11, "J": 10, "T": 9, "9": 8, "8": 7, "7": 6, "6": 5, "5": 4, "4": 3, "3": 2, "2": 1}
 
 
 def parse_content(cnt):
@@ -18,32 +17,33 @@ def parse_content(cnt):
 def value_of_card(cards: str):
     counts = Counter(cards)
     if 5 in counts.values():
-        main = 1
+        main = 7
     elif 4 in counts.values():
-        main = 2
+        main = 6
     elif 3 in counts.values() and 2 in counts.values():
-        main = 3
+        main = 5
     elif 3 in counts.values():
         main = 4
     elif len([e for e in counts.values() if e == 2]) == 2:
-        main = 5
+        main = 3
     elif 2 in counts.values():
-        main = 6
+        main = 2
     else:
-        main = 7
+        main = 1
     card_ranking = tuple([main] + [ORDER[c] for c in cards])
     return cards, card_ranking
 
 
 def question_1(lines: list[tuple[str, int]]) -> int:
     card_values = [(value_of_card(card), bid) for card, bid in lines]
-    sorted_cards = sorted(card_values, key=lambda x: x[0][1], reverse=True)
+    sorted_cards = sorted(card_values, key=lambda x: x[0][1])
     order_of_strengths = sum([(idx+1)*bid for idx, (_, bid) in enumerate(sorted_cards)])
     return order_of_strengths
 
 
 @helper.profiler
 def question_2(lines: list[tuple[str, int]]) -> int:
+    card_values = [(value_of_card(card), bid) for card, bid in lines]
     return result
 
 
