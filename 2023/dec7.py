b2cc3609895a0c412390f8ad064aafb4c5a7a391
mstaal <m@8.dk>
2023-12-10 23:41:19 +0100

    Day 7, refactored

--- 2023/dec7.py
+++ 2023/dec7.py
@@ -5,31 +5,24 @@ from collections import Counter
 
 
 ORDER = {"A": 13, "K": 12, "Q": 11, "J": 10, "T": 9, "9": 8, "8": 7, "7": 6, "6": 5, "5": 4, "4": 3, "3": 2, "2": 1}
-ORDER_J = {k: v for k, v in ORDER.items() if k != "J"}
-
-
-def parse_content(cnt):
-    parsed = [(c[0], int(c[1])) for c in [[x for x in c.split(" ")] for c in cnt]]
-    return parsed
+ORDER_J = {k: v if k != "J" else 0 for k, v in ORDER.items()}
 
 
 def main_value_of_card(cards: str):
     counts = Counter(cards)
     if 5 in counts.values():
-        main = 7
+        return 7
     elif 4 in counts.values():
-        main = 6
+        return 6
     elif 3 in counts.values() and 2 in counts.values():
-        main = 5
+        return 5
     elif 3 in counts.values():
-        main = 4
+        return 4
     elif len([e for e in counts.values() if e == 2]) == 2:
-        main = 3
+        return 3
     elif 2 in counts.values():
-        main = 2
-    else:
-        main = 1
-    return main
+        return 2
+    return 1
 
 
 def value_of_card(cards: str):
@@ -39,15 +32,12 @@ def value_of_card(cards: str):
 def value_of_card_j(cards: str):
     hand_without_js = "".join([c for c in cards if c != "J"])
     hands = [f"""{hand_without_js}{"".join(com)}""" for com in product(list(ORDER_J.keys()), repeat=cards.count("J"))]
-    main = max([main_value_of_card(h) for h in hands])
-    card_ranking = tuple([main] + [ORDER_J.get(c, 0) for c in cards])
-    return card_ranking
+    return tuple([max([main_value_of_card(h) for h in hands])] + [ORDER_J[c] for c in cards])
 
 
 def compute_total_winning(lines: list[tuple[str, int]], card_ranking: callable) -> int:
-    card_values = [(card, card_ranking(card), bid) for card, bid in lines]
-    sorted_cards = sorted(card_values, key=lambda x: x[1])
-    order_of_strengths = sum([(idx + 1) * bid for idx, (_, _, bid) in enumerate(sorted_cards)])
+    card_values = [(card_ranking(card), bid) for card, bid in lines]
+    order_of_strengths = sum([(idx + 1) * bid for idx, (_, bid) in enumerate(sorted(card_values))])
     return order_of_strengths
 
 
@@ -61,7 +51,7 @@ def question_2(lines: list[tuple[str, int]]) -> int:
 
 
 if __name__ == '__main__':
-    content = parse_content(Path("data/day7.txt").read_text(encoding="UTF-8").split("\n"))
+    content = [(c[0], int(c[1])) for c in [[x for x in c.split(" ")] for c in Path("data/day7.txt").read_text(encoding="UTF-8").split("\n")]]
 
     question1 = question_1(content)
     print(f"Result 1: {str(question1)}")
