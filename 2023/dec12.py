132789d308944083e69f4bd37536beaf827803e8
mstaal <m@8.dk>
2023-12-12 08:00:42 +0100

    Day 11, p1

---
+++ 2023/dec12.py
@@ -0,0 +1,58 @@
+from utils import aoc_helper as helper
+from pathlib import Path
+import re
+from functools import cache, lru_cache
+
+
+class TList:
+    def __init__(self, lst):
+        self.lst = lst
+
+    def __hash__(self):
+        return hash(tuple(self.lst))
+
+
+def parse_input(parsed):
+    parts = [line.split(" ") for line in parsed]
+    parts = [(part[0], [int(e) for e in part[1].split(",")]) for part in parts]
+    return parts
+
+
+def count_combinations(text, numbers):
+    groups = [m.span()[0] for m in re.compile(r"\?").finditer(text)]
+    replacements = [text]
+    tmp = []
+    for group in groups:
+        for t in replacements:
+            first = t[:group] + "#" + t[group+1:]
+            second = t[:group] + "." + t[group+1:]
+            tmp.append(first)
+            tmp.append(second)
+        replacements = tmp
+        tmp = []
+    replacements = [t for t in replacements if t.count("#") == sum(numbers)]
+    replacements_split = [(idx, [len(e) for e in t.split(".") if e != ""]) for idx, t in enumerate(replacements)]
+    replacements_split = [idx for idx, t in replacements_split if t == numbers]
+    replacements = [replacements[idx] for idx in replacements_split]
+    return replacements
+
+
+@helper.profiler
+def question_1(digested) -> int:
+    combs = [count_combinations(text, numbers) for text, numbers in digested]
+    total = sum([len(comb) for comb in combs])
+    return total
+
+
+@helper.profiler
+def question_2(parsed) -> int:
+    return 2
+
+
+if __name__ == '__main__':
+    parsed = Path("data/day12.txt").read_text(encoding="UTF-8").split("\n")
+    digested = parse_input(parsed)
+    q1 = question_1(digested)
+    print(f"Result 1: {str(q1)}")
+    q2 = question_2(parsed)
+    print(f"Result 2: {str(q2)}")
