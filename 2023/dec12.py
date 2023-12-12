160a12568441bee7947c05b21b876bc8b5cbb72c
mstaal <m@8.dk>
2023-12-12 08:14:08 +0100

    Day 11, p1

--- 2023/dec12.py
+++ 2023/dec12.py
@@ -21,15 +21,8 @@ def parse_input(parsed):
 def determine_combinations(text, numbers):
     groups = [m.span()[0] for m in re.compile(r"\?").finditer(text)]
     replacements = [text]
-    tmp = []
     for group in groups:
-        for t in replacements:
-            first = t[:group] + "#" + t[group+1:]
-            second = t[:group] + "." + t[group+1:]
-            tmp.append(first)
-            tmp.append(second)
-        replacements = tmp
-        tmp = []
+        replacements = [itm for t in replacements for itm in [t[:group] + "#" + t[group+1:], t[:group] + "." + t[group+1:]]]
     replacements = [t for t in replacements if t.count("#") == sum(numbers)]
     replacements_split = [(idx, [len(e) for e in t.split(".") if e != ""]) for idx, t in enumerate(replacements)]
     replacements_split = [idx for idx, t in replacements_split if t == numbers]
