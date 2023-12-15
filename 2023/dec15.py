4f3a2dbc4e56d6d234ee0b64b01c6ec30cb6a6a8
mstaal <m@8.dk>
2023-12-15 10:52:01 +0100

    Day 15, refactor

--- 2023/dec15.py
+++ 2023/dec15.py
@@ -8,7 +8,7 @@ from functools import cache
 def hasher(element):
     current_value = 0
     for ascii_code in (ord(e) for e in element):
-        current_value = ((current_value + ascii_code) * 17) % 256
+        current_value = (current_value + ascii_code) * 17 % 256
     return current_value
 
 
@@ -20,12 +20,12 @@ def question_1(p) -> int:
 @helper.profiler
 def question_2(p) -> int:
     boxes = defaultdict(dict)
-    for idx, elm_x in enumerate(p):
-        if "=" in elm_x:
-            label, focal_length = elm_x.split("=")
+    for line in p:
+        if "=" in line:
+            label, focal_length = line.split("=")
             boxes[hasher(label)][label] = int(focal_length)
-        if "-" in elm_x:
-            label = elm_x[:-1]
+        if "-" in line:
+            label = line[:-1]
             if hasher(label) in boxes and label in boxes[hasher(label)]:
                 del boxes[hasher(label)][label]
     return sum((box+1)*(slot+1)*length for box in boxes.keys() for slot, length in enumerate(boxes[box].values()))
